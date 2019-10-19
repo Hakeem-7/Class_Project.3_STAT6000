543588e925a4df4a90162ff3581b11052b36eb78
Hakeem-7 <52264207+Hakeem-7@users.noreply.github.com>
2019-10-19 15:17:01 -0500

    Alternative ways of splitting a data set.

--- Alternatives.R
+++ Alternatives.R
@@ -46,7 +46,7 @@ install.packages("caTools")
 
 ## Splitting sample size evenly
 
-smp.size <- floor(.90*nrow(Auto))
+smp.size <- floor(.50*nrow(Auto))
 set.seed(107) #To make the partitioning reproducible
 train.ind <- sample(seq_len(nrow(Auto)), size = smp.size)
 Auto.train1 <- Auto[train.ind, ]
@@ -59,13 +59,30 @@ lda.fit2.1
 lda.pred2.1 = predict(lda.fit2.1, Auto.test1)
 table(lda.pred2.1$class,mpg01.test1)
 mean(lda.pred2.1$class!=mpg01.test1)
-## Test error is approx.10.2%
+## Test error is approx.9.2%
 
 # The reason for the disparity in model accuracy can be traced to the differences in the data frame used.
+# Alternative 1 is reasonable if the entire data is to be splitted into 2.
+
 
 fix(Auto)
 
 
 # Alternative method 2
 
+require(caTools)
+set.seed(107)
+train2 = sample.split(Auto$year, SplitRatio = .50) #Spltting the dat w/ diff. col. yields dif. test error estimate?? 
+Auto.train2 = subset(Auto, train2 == TRUE)
+Auto.test2 = subset(Auto, train2 == FALSE) #instead of using the "==" sign
+mpg01.test2 = mpg01[!train2]
 
+lda.fit2.2 = lda(mpg01 ~ weight+acceleration+horsepower+displacement+year, 
+                 data = Auto, subset = train2)
+lda.fit2.2
+lda.pred2.2 = predict(lda.fit2.2, Auto.test2)
+table(lda.pred2.2$class,mpg01.test2)
+mean(lda.pred2.2$class!=mpg01.test2)
+## Test error is approx.11.2%.
+
+# Alternative 2 is reasonable if the splitting is desirable in a particular column.
